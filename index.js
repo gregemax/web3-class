aaa8b7e3cc2e13b60fa775a32faec794a83f10c5
gregemax <gregemax700@gmail.com>
2025-06-30 10:28:57 +0100

    done

--- index.js
+++ index.js
@@ -1,64 +1,34 @@
-const { ec: EC } = require('elliptic');
+const EC = require('elliptic').ec;
 const crypto = require('crypto');
 
 const ec = new EC('secp256k1');
 
 
-function generateKeyPair() {
-  const key = ec.genKeyPair();
-  return {
-    privateKey: key.getPrivate('hex'),
-    publicKey: key.getPublic('hex'),
-    keyObject: key
-  };
-}
-
-
-function generateAddress(publicKey) {
-  return crypto.createHash('sha256').update(publicKey).digest('hex');
-}
-
-
-function createMessage(firstName, lastName) {
-  return `My name is ${firstName} ${lastName}`;
-}
-
-
-function hashMessage(message) {
-  return crypto.createHash('sha256').update(message).digest();
-}
-
-
-function signMessage(key, messageHash) {
-  const signature = key.sign(messageHash);
-  return signature.toDER('hex');
-}
-
-
-function verifySignature(publicKeyHex, messageHash, signatureHex) {
-  const publicKey = ec.keyFromPublic(publicKeyHex, 'hex');
-  return publicKey.verify(messageHash, signatureHex);
-}
-
-
-
-const { privateKey, publicKey, keyObject } = generateKeyPair();
+const key = ec.genKeyPair();
+const privateKey = key.getPrivate('hex');
 console.log('Private Key:', privateKey);
+
+
+const publicKey = key.getPublic('hex');
 console.log('Public Key:', publicKey);
 
-const address = generateAddress(publicKey);
+
+const address = crypto.createHash('sha256').update(publicKey).digest('hex');
 console.log('Address:', address);
 
 const firstName = 'Emmanuel';
 const lastName = 'Greg';
-const message = createMessage(firstName, lastName);
+const message = `My name is ${firstName} ${lastName}`;
 console.log('Message:', message);
 
-const messageHash = hashMessage(message);
+
+const messageHash = crypto.createHash('sha256').update(message).digest();
 console.log('Message Hash:', messageHash.toString('hex'));
 
-const signatureHex = signMessage(keyObject, messageHash);
+
+const signature = key.sign(messageHash);
+const signatureHex = signature.toDER('hex');
 console.log('Digital Signature:', signatureHex);
 
-const isValid = verifySignature(publicKey, messageHash, signatureHex);
+const isValid = ec.keyFromPublic(publicKey, 'hex').verify(messageHash, signature);
 console.log('Signature Valid:', isValid);
